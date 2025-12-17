931524a52bdd40a030afabf2dc82e0bf27f06181
mauger12 <71387304+mauger12@users.noreply.github.com>
2025-12-17 02:16:06 +0000

    Adds TeamColumn function for code reuse

--- app/src/main/java/com/example/fourthofficial/ui/SetupScreen.kt
+++ app/src/main/java/com/example/fourthofficial/ui/SetupScreen.kt
@@ -3,81 +3,57 @@ package com.example.fourthofficial.ui
 import androidx.compose.material3.Text
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.ui.Alignment
 
+data class Team(
+    val name: String,
+    val players: List<String>
+)
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SetupScreen(modifier: Modifier = Modifier) {
+    var team1 by remember { mutableStateOf(Team("Team 1", List(23) { "Player ${it+1}" })) }
+    var team2 by remember { mutableStateOf(Team("Team 2", List(23) { "Player ${it+1}" })) }
+
     Row(
         modifier = modifier
             .fillMaxSize()
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            modifier = Modifier
-                .padding(5.dp)
-                .weight(1f)
-                .fillMaxHeight()
+        TeamColumn(
+            team = team1,
+            modifier = Modifier.weight(1f)
+        )
+        TeamColumn(
+            team = team2,
+            modifier = Modifier.weight(1f)
+        )
+    }
+}
+
+@Composable
+fun TeamColumn(team: Team, modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier
+            .fillMaxHeight()
+            .padding(5.dp))
+    {
+        Text(team.name, style = MaterialTheme.typography.headlineMedium)
+        LazyColumn(
+            modifier = Modifier.weight(1f)
         ) {
-            Text("Team 1", style = MaterialTheme.typography.headlineMedium)
-            LazyColumn(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
-                    .weight(1f)
-                    .fillMaxWidth()
-            ) {
-                items(23) { i ->
-                    Text("Player ${i + 1}", modifier = Modifier.padding(2.dp))
-                }
-            }
-
-            Spacer(modifier = Modifier.height(24.dp))
-
-            Button(
-                onClick = { },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 8.dp)
-            ) {
-                Text("Edit Team")
-            }
+            items(team.players.size) { i -> Text(team.players[i]) }
         }
-
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            modifier = Modifier
-                .padding(5.dp)
-                .weight(1f)
-                .fillMaxHeight()
-        ) {
-            Text("Team 2", style = MaterialTheme.typography.headlineMedium)
-            LazyColumn(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
-                    .weight(1f)
-                    .fillMaxWidth()
-            ) {
-                items(23) { i ->
-                    Text("Player ${i + 1}", modifier = Modifier.padding(2.dp))
-                }
-            }
-
-            Spacer(modifier = Modifier.height(24.dp))
-
-            Button(
-                onClick = { },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 8.dp)
-            ) {
-                Text("Edit Team")
-            }
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(onClick = { }, modifier = Modifier.fillMaxWidth()) {
+            Text("Edit Team")
         }
     }
 }
