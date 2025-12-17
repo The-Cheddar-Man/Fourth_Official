dafb09560a05278342a2a7e0e1d176c8011e3fc1
mauger12 <71387304+mauger12@users.noreply.github.com>
2025-12-17 01:47:13 +0000

    Adds team columns and basic UI to setup screen

--- app/src/main/java/com/example/fourthofficial/ui/SetupScreen.kt
+++ app/src/main/java/com/example/fourthofficial/ui/SetupScreen.kt
@@ -7,15 +7,78 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.ui.Alignment
 
 @Composable
 fun SetupScreen(modifier: Modifier = Modifier) {
-    Column(
+    Row(
         modifier = modifier
             .fillMaxSize()
-            .padding(16.dp)
     ) {
-        Text("Setup Screen", style = MaterialTheme.typography.headlineMedium)
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier
+                .padding(5.dp)
+                .weight(1f)
+                .fillMaxHeight()
+        ) {
+            Text("Team 1", style = MaterialTheme.typography.headlineMedium)
+            LazyColumn(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
+            ) {
+                items(23) { i ->
+                    Text("Player ${i + 1}", modifier = Modifier.padding(2.dp))
+                }
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button(
+                onClick = { },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp)
+            ) {
+                Text("Edit Team")
+            }
+        }
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier
+                .padding(5.dp)
+                .weight(1f)
+                .fillMaxHeight()
+        ) {
+            Text("Team 2", style = MaterialTheme.typography.headlineMedium)
+            LazyColumn(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
+            ) {
+                items(23) { i ->
+                    Text("Player ${i + 1}", modifier = Modifier.padding(2.dp))
+                }
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button(
+                onClick = { },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp)
+            ) {
+                Text("Edit Team")
+            }
+        }
     }
 }
 
